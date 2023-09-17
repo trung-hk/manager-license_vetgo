bcbe00c44871cee894577818ddc30527ab1dfc1d
Phat <nguyenphatit.nl@gmail.com>
2023-09-17 14:15:59 +0700

    curd agents page

--- panels/src/app/models/agent.ts
+++
@@ -1,9 +0,0 @@
-export class Agent {
-    id: string | null | undefined;
-    code!: string;
-    name!: string;
-    phone!: string;
-    email!: string;
-    address!: string;
-    status!: number;
-}
